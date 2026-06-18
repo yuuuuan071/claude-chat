c2f6aed77059127ad64008f1d471eb1d6f188219
yuuuuan <3163752556@qq.com>
2026-06-18 19:08:52 +0800

    feat: PWA manifest和图标, 时间感知, 删除对话同步DB

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#1a1f2e" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
